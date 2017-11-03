48bf10e9371fdb70b8f6ac367088839ac1588463
Rodrigo <rodrigo.redcode@gmail.com>
2017-11-03 18:34:18 -0200

    Add tabulation at generated code and add some examples of brainfuck code

--- compiler.py
+++ compiler.py
@@ -10,6 +10,7 @@ args = parser.parse_args()
 INPUT_FILENAME = args.filename
 OUTPUT_FILENAME = args.o
 
+TAB_NUMBERS = 0
 
 def main():
     arquivobf = open(INPUT_FILENAME, 'r')
@@ -25,18 +26,20 @@ def main():
     arquivobf.close()
     arquivojs.close()
 
-
-
-
 def convert_to_js(char):
-    text = ''
+    global TAB_NUMBERS
+    loop_tab_numbers = TAB_NUMBERS
+    if char == ']':
+        loop_tab_numbers -= 1
+    text = create_tabs(loop_tab_numbers)
 
     if(char == '+'):
         text += 'fita[i]++\n'
     elif(char == "-"):
         text += 'fita[i]--\n'
     elif(char == ">"):
-        text += 'i++\nif (isNaN(fita[i])) fita[i]=0\n'
+        text += 'i++\n'
+        text += create_tabs(TAB_NUMBERS) + 'if (isNaN(fita[i])) fita[i]=0\n'
     elif(char == "<"):
         text += 'i--\n'
     elif(char == '.'):
@@ -44,16 +47,22 @@ def convert_to_js(char):
     elif(char == ','):
         text += 'fita[i] = window.prompt().charCodeAt(0)\n'
     elif(char == '['):
-        text += 'while(fita[i] != 0){'
+        TAB_NUMBERS += 1
+        text += 'while(fita[i] != 0){\n'
     elif(char == ']'):
-        text += '}'
+        TAB_NUMBERS -= 1
+        text.replace('\t', '')
+        text += '}\n'
 
     return text
-
-
 
 def initialize(arquivojs):
     arquivojs.write("var fita = [0]\nvar i = 0\n\n")
 
+def create_tabs(tab_numbers):
+    tabs = ''
+    for x in range(0, tab_numbers):
+        tabs += '\t'
+    return tabs
 
 main()
